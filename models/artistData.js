eb11a96d9027f4e40d73e004d976731b8f51813e
Samrat Mukherjee <samrat.mukherjee@sap.com>
2024-01-20 18:55:34 +0530

    artist data modif

--- models/artistData.js
+++ models/artistData.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
+const reviewSchema = new mongoose.Schema({
+  customerName: { type: String, required: false },
+  rating: { type: Number, required: false, min: 1, max: 5 },
+  review: { type: String, required: false },
+});
+
 const artistDataScheme = new mongoose.Schema({
   name: { type: String, required: true },
   location: { type: String, required: true },
@@ -8,13 +14,13 @@ const artistDataScheme = new mongoose.Schema({
   socialMediaLinks: [{ type: String, required: true }],
   bio: { type: String, required: true },
   experience: { type: String, required: true },
-  tags: { type: String, required: true },
-  availability: { type: String, required: true },
+  tags: [{type: String, required: true}],
+  availability: [{ type: String, required: true }],
   perInchPrice: { type: String, required: true },
-  inkTypesAvailable: { type: String, required: true },
+  inkTypesAvailable: [{ type: String, required: true }],
   portfolio: [{ type: String, required: true }],
   featuredWork: [{ type: String, required: false }],
-  customerReviews: [{ type: String, required: false}]
+  customerReviews: [reviewSchema],
 });
 
-module.exports = mongoose.model("ArtistData", artistDataScheme);
+module.exports = mongoose.model("artist", artistDataScheme);
